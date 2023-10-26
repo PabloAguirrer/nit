9378ebf6185b01e297d36e07444883255b881580
PabloAguirrer <144390944+PabloAguirrer@users.noreply.github.com>
2023-10-25 21:37:37 -0300

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,7 @@
+function pop() {alert("Desarrollador: Pablo aguirre, Dirección: Carlos Gimenez 819, Cel: 291-500-5200" )}
+
+function modonocturno() {document.body.style.backgroundColor="black";}
+function modonocturno1() {document.body.style.backgroundColor="white";}
+
+
+
